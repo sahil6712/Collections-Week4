a5c8df908dbe51fed312597fba3d008a4f320fd8
Sahil <sahilkhemariya327@gmail.com>
2025-02-12 13:04:25 +0530

    [Sahil] Add. Program to rotate a List

---
+++ day2/src/main/java/org/example/list/RotateElements.java
@@ -0,0 +1,42 @@
+package org.example.list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RotateElements {
+
+    // Method to rotate the list
+    public static <T> List<T> rotateList(List<T> list, int numberOfPos) {
+
+        numberOfPos = numberOfPos%(list.size());   // If there are number of rotation greater then array size
+
+        // iterate the list
+        int i=0;
+        while(i<numberOfPos) {
+            list.add(list.size(),list.get(0));
+            list.remove(0);
+            i++;
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+
+        // Number of positions to rotate
+        int numberOfPos = 2;
+
+        // List
+        List<Integer> list = new ArrayList<>(Arrays.asList(32,45,27,81,90,108,18));
+
+        // Display the list
+        System.out.println("List: "+list);
+
+        // Call the method to get the rotated list
+        rotateList(list, numberOfPos);
+
+        // Display the rotated list
+        System.out.println("Rotated List: "+list);
+
+
+    }
+}
---
+++ day2/src/test/java/ListTest/RotateElementsTest.java
@@ -0,0 +1,32 @@
+package ListTest;
+
+import org.example.list.RotateElements;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RotateElementsTest {
+    static List<Integer> ls = new ArrayList<>();
+
+    @BeforeAll
+    static void setup() {
+        ls.add(3);
+        ls.add(4);
+        ls.add(5);
+        ls.add(6);
+        ls.add(1);
+        ls.add(2);
+    }
+
+    @Test
+    void rotateTest() {
+
+        assertEquals(RotateElements.rotateList(new ArrayList<>(Arrays.asList(1,2,3,4,5,6)),2), ls);
+    }
+
+}
