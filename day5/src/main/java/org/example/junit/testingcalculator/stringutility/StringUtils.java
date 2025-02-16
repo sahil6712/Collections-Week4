e66dd5f353de2247acffc48194120909d48b8f7b
Sahil <sahilkhemariya327@gmail.com>
2025-02-16 15:24:58 +0530

    [Sahil] Add. Program to test a string utility

---
+++ day5/src/main/java/org/example/junit/testingcalculator/stringutility/StringUtils.java
@@ -0,0 +1,30 @@
+package org.example.junit.testingcalculator.stringutility;
+
+public class StringUtils {
+    public String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public boolean isPalindrome(String str) {
+        String reversed = reverse(str);
+        return str.equalsIgnoreCase(reversed);
+    }
+
+    public String toUpperCase(String str) {
+        return str.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+
+        // String
+        String str = "Sahil";
+
+        // Object of the class
+        StringUtils strUtility = new StringUtils();
+
+        // Call methods to perform operations on string
+        System.out.println("Reversed string is: "+strUtility.reverse(str));
+        System.out.println("Is string palindroe: "+strUtility.isPalindrome(str));
+        System.out.println("Upper case string: "+strUtility.toUpperCase(str));
+    }
+}
