9bd6491790a24a83837f5e256b97404ccd2e9764
Sahil <sahilkhemariya327@gmail.com>
2025-02-16 11:18:33 +0530

    [Sahil] Add. Program of exception propagation

---
+++ day4/src/main/java/org/example/exceptionpropagation/ExceptionPropagation.java
@@ -0,0 +1,22 @@
+package org.example.exceptionpropagation;
+
+public class ExceptionPropagation {
+    public static void method1() {
+        throw new ArithmeticException("Division by zero");
+    }
+
+    public static void method2() {
+        method1();
+    }
+
+    public static void main(String[] args) {
+        try {
+            method2();
+        } catch (ArithmeticException e) {
+            System.out.println("Handled exception in main");
+        }
+    }
+}
+
+
+
