90dd921c3fe003f46cbb03d85875fd26ae0dd4af
Sahil <sahilkhemariya327@gmail.com>
2025-02-17 23:30:06 +0530

    [Sahil] Refactor. Added to package reflection

---
+++ day6/src/main/java/org/example/Reflection/basiclevel/invokeprivatemethod/Calculator.java
@@ -0,0 +1,11 @@
+package org.example.Reflection.basiclevel.invokeprivatemethod;
+
+import java.lang.reflect.Method;
+
+class Calculator {
+    // Private method
+    private int multiply(int a, int b) {
+        return a * b;
+    }
+}
+
