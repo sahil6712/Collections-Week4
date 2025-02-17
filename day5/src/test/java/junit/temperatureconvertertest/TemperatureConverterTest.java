978668bd866e8922b44c180523ca4391b3e3f46f
Sahil <sahilkhemariya327@gmail.com>
2025-02-17 22:56:37 +0530

    [Sahil] Add. Temperature Converter

---
+++ day5/src/test/java/junit/temperatureconvertertest/TemperatureConverterTest.java
@@ -0,0 +1,20 @@
+package junit.temperatureconvertertest;
+
+import org.example.junit.temperatureconverter.TemperatureConverter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TemperatureConverterTest {
+
+    @Test
+    void testCelsiusToFahrenheit() {
+        assertEquals(32.0, TemperatureConverter.celsiusToFahrenheit(0), 0.01, "0°C should be 32°F");
+    }
+
+    @Test
+    void testFahrenheitToCelsius() {
+        assertEquals(0.0, TemperatureConverter.fahrenheitToCelsius(32), 0.01, "32°F should be 0°C");
+
+    }
+}
